e8639f3599f1e20e9d9a92d4017df9218b020f58
Dev Luca <lucahesper@hotmail.com>
2022-12-19 11:03:00 -0300

    base design system

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,30 @@
-import { colors } from '@ignite-ui/tokens'
+import { ComponentProps } from 'react'
+import { styled } from './styles'
 
-export function app() {
-  return (
-    <div>
-      <h1 style={{ color: colors.ignite300 }}>Hello world</h1>
-    </div>
-  )
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  background: '$ignite300',
+  color: '$white',
+  border: 0,
+  borderRadius: '$sm',
+  cursor: 'pointer',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
